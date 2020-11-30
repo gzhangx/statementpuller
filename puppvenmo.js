8c57bf0ed1e61fcb74f1f97c684f2629fc6889c2
Gang Zhang <gzhangx@hotmail.com>
2020-11-29 23:50:40 -0500

    temp

--- puppvenmo.js
+++ puppvenmo.js
@@ -4,6 +4,7 @@ const { sleep,
     waitElement,
     readOneLine,
 } = require('./lib/util');
+const moment = require('moment');
 const creds = require('./creds.json');
 const { Promise } = require('bluebird');
 async function test(creds) {
@@ -13,22 +14,23 @@ async function test(creds) {
     });
     
     //const cookies = JSON.parse(fs.readFileSync('outputData/chrcookies.json'));
-    //pupp.page.setCookie(cookies);
-    await pupp.loadCookies();
+    //pupp.page.setCookie(cookies);    
         
     await pupp.goto('https://venmo.com/account/sign-in'); //creds.url
+    await pupp.loadCookies();
     console.log('opened');
     let tryNum = 0;
     while (true) {
         try {
             //(await pupp.$('a.sign-in.active')).click();
+            await sleep(1000);
             console.log('went to active, gong user try ' + tryNum  );
             tryNum++;
             await pupp.setTextByName('phoneEmailUsername', creds.userName);            
             await pupp.setTextByName('password', creds.password);            
             const btn = await pupp.$('.auth-button');
             console.log('auth clock');
-            await sleep(4000);
+            await sleep(4500);
             await btn.click();
             console.log('auth clicked');
         } catch {
@@ -40,7 +42,7 @@ async function test(creds) {
     const saveScreenshoot = ()=>pupp.screenshot('outputData/test.png');
 
     const loggedInCheck = async () => {
-        const jl = await pupp.findBySelector("//*[text()[contains(.,'Jinlin Xie')]]");
+        const jl = await pupp.findByXPath("//*[text()[contains(.,'Jinlin Xie')]]");
         if (!jl) throw { message: 'not ready' };
     }
     const CheckCode = async () => {
@@ -58,14 +60,19 @@ async function test(creds) {
             action: async () => {
                 const codeField = await pupp.findBySelector('.auth-form-input');
                 const code = await readOneLine('Please input code');
-                codeField.sendKeys(code);
+                pupp.setText('.auth-form-input', code);
                 let btn = await pupp.findBySelector('.auth-button');
-                btn.click();
                 await sleep(1000);
-
-                const NoRemember = await pupp.findBySelector('.mfa-button-do-not-remember');
-                btn = await driver.findElement(By.css('.auth-button'));
                 btn.click();
+                await sleep(3000);
+
+                //document.querySelector("#content > div > div > div > form > div > button.ladda-button.auth-button")
+                ////*[@id="content"]/div/div/div/form/div/button[1]
+                //#content > div > div > div > form > div > button.ladda-button.auth-button
+                //const NoRemember = await pupp.findBySelector('.mfa-button-do-not-remember');
+                btn = await pupp.findBySelector('button.ladda-button.auth-button');
+                btn.click();
+                await sleep(3000);
             }
         });
 
@@ -98,6 +105,32 @@ async function test(creds) {
     //const cookies = await pupp.page.cookies();
     //await fs.writeFile('outputData/test.png', JSON.stringify(cookies, null, 2));
     await pupp.saveCookies();
+    await sleep(10000);
+
+
+
+    //step2
+    const statementUrl = `https://venmo.com/account/statement?end=${moment().format('YYYY-MM-DD')}&start=${moment().add(-89, 'days').format('YYYY-MM-DD')}`;
+    console.log(statementUrl);
+    await saveScreenshoot();
+    pupp.goto(statementUrl);
+    await sleep(2000);
+    await saveScreenshoot();
+    await waitElement({
+        message: 'Wait entrance',
+        waitSeconds: 5,
+        action: async () => {
+            await saveScreenshoot();
+            await sleep(1000);
+            const tran = await pupp.findByXPath("//*[text()[contains(.,'Completed Transactions')]]");
+            if (!tran) throw {message:'need tran'}
+        }
+    });
+
+
+    console.log('done');
+    await sleep(10000);
+    await saveScreenshoot();
     //html body div#app div div div#content div.container-fluid div form.auth-form fieldset.inputs label.error.auth-form-input-label input.auth-form-input
     await pupp.close();
 }
